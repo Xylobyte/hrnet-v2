50a9e0279c54d84fd24792ccb5f6f2f0e537dca7
Nantsa Montillet <nantsamontillet@xylobyte.com>
2025-06-30 20:36:03 +0200

    Add hooks, add page title, add base react libs

--- src/pages/EmployeeList.tsx
+++ src/pages/EmployeeList.tsx
@@ -1,6 +1,11 @@
 import './EmployeeList.scss'
+import { useEffect } from 'react'
 
 function EmployeeList() {
+	useEffect(() => {
+		document.title = 'HRnet - Employees list'
+	}, [])
+
 	return <div></div>
 }
 
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,6 +1,11 @@
 import './Home.scss'
+import { useEffect } from 'react'
 
 function Home() {
+	useEffect(() => {
+		document.title = 'HRnet - Home'
+	}, [])
+
 	return (
 		<>
 			<h2>Create Employee</h2>
